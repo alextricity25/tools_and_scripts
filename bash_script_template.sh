4039fa706eee6ccce8d98ae190f7484143749abe
Miguel Alex Cantu <miguel.can2@gmail.com>
2017-11-06 17:03:16 -0800

    Added bash script template

---
+++ bash_script_template.sh
@@ -0,0 +1,30 @@
+#-----------------------------------------------------------------------------
+#
+# Name: <name>
+# Description:
+# Write a short description of what the program does here.
+# Author: Alex Cantu
+# Date: 10/21/2017
+#
+#
+#-----------------------------------------------------------------------------
+
+
+# VARS -----------------------------------------------------------------------
+## String variable
+SOME_VARIABLE='HELLO WORLD'
+## Int variable
+INT_VARIABLE=23
+
+
+# MAIN -----------------------------------------------------------------------
+
+
+
+
+# FUNCTION: <name>
+# Description:
+# Some description of function here
+some_function() {
+    echo "Some function!"
+}
